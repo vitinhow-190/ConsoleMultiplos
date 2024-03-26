bdba586d015908e9ca0143f51b61a88b87985de9
Usuario <Usuario@CPD02-M07>
2024-03-26 10:47:27 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleMultiplos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMultiplos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.WriteLine("Digite o numero desejado: ");
+            int mult = int.Parse(Console.ReadLine());
+
+            int cont = mult;
+
+            while (cont <= 100) 
+            {
+                Console.Write(cont);
+
+                cont += mult;
+            
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
